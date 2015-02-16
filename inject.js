886b24ab47df98a0a931def0c0d28a3531591d8c
Sungwoo Choo <choo@sungwoo.me>
2015-02-16 18:36:37 +0900

    매크로 시작.중지시 나오는 메세지를 한글로 변경

--- inject.js
+++ inject.js
@@ -1,11 +1,11 @@
 
 function macro() {
-	alert("Now starting macro.");
+	alert("매크로를 시작합니다.");
 	localStorage.setItem('macro', true);
 	inqSchedule();
 }
 
 function macrostop() {
-	alert("Now stopping macro.");
+	alert("매크로를 중지합니다.");
 	localStorage.removeItem('macro');
 }
