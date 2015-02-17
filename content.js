ca5c9d76b49f3ea78efe24d646d873f84ea71818
Sungwoo Choo <choo@sungwoo.me>
2015-02-17 11:54:02 +0900

    버튼 Look&Feel을 새로운 letskorail.com 에 맞춰서 변경 결과페이지가 정상적으로 나오지 않은 경우 (오류메세지 등)에 대한 처리 추가 조회 시작시 alert에서 안내메세지에서 5분이내 결제해야한다는 메세지 추가

--- content.js
+++ content.js
@@ -18,42 +18,47 @@ if (document.URL.substring(0, dsturl1.length) == dsturl1 ||
 	injectJs(chrome.extension.getURL('inject.js'));
 
 		if (localStorage.getItem('macro') == "true") {
-			$(".btn_inq").append('<a href="#" onclick="macrostop();" style="font-size:15px; color:stop;"><img src="' + chrome.extension.getURL('macrostop.gif') + '"></a>');
+			$(".btn_inq").append('<a href="#" onclick="macrostop();" style="font-size:15px; margin-left:5px;"><img src="' + chrome.extension.getURL('btn_stop.png') + '"></a>');
 			
 		} else {
-			$(".btn_inq").append('<a href="#" onclick="macro();" style="font-size:15px; color:green;"><img src="' + chrome.extension.getURL('macro.gif') + '"></a>');
+			$(".btn_inq").append('<a href="#" onclick="macro();" style="font-size:15px; margin-left:5px;"><img src="' + chrome.extension.getURL('btn_start.png') + '"></a>');
 		}
 
 		if (localStorage.getItem('macro') == "true") {
-			var rows = $('#divResult > table.tbl_h tr');
 
-			var succeed = false;
-			for (i = 1; i < rows.length; i++) {
-				var columns = $(rows[i]).children('td');
-				var specials = $(columns[5]).children("a");
-				if (specials.length != 0) {
-					for (j = 0; j < specials.length; j++) {
-						img = $(specials[j]).children('img');
-						src = $(img).attr('src');
-						if (src == "/docs/2007/img/common/icon_apm_yes.gif") {
-							location.href = $(specials[j]).attr('href');
-							succeed = true;
-							break;
+			if ($("#divResult").length != 0) {
+				var rows = $('#divResult > table.tbl_h tr');
+
+				var succeed = false;
+				for (i = 1; i < rows.length; i++) {
+					var columns = $(rows[i]).children('td');
+					var specials = $(columns[5]).children("a");
+					if (specials.length != 0) {
+						for (j = 0; j < specials.length; j++) {
+							img = $(specials[j]).children('img');
+							src = $(img).attr('src');
+							if (src == "/docs/2007/img/common/icon_apm_yes.gif") {
+								location.href = $(specials[j]).attr('href');
+								succeed = true;
+								break;
+							}
 						}
+						if (succeed == true) break;
 					}
-					if (succeed == true) break;
 				}
-			}
 
-			if (succeed == true) {
-				localStorage.removeItem('macro');
-				chrome.extension.sendMessage({type: 'playSound'}, function(data) { });
+				if (succeed == true) {
+					localStorage.removeItem('macro');
+					chrome.extension.sendMessage({type: 'playSound'}, function(data) { });
+				} else {
+					// 모두 실패한 경우
+					setTimeout(function() { 
+					location.href = "javascript:inqSchedule();";
+					}, 500);
+				}
 			} else {
-				// 모두 실패한 경우
-				setTimeout(function() { 
-				location.href = "javascript:inqSchedule();";
-				}, 500);
-				
+				// 결과폼이 없는 경우 (오류 화면 발생?)
+				history.go(-1);
 			}
 		}
 	});
--- inject.js
+++ inject.js
@@ -1,11 +1,11 @@
 
 function macro() {
-	alert("매크로를 시작합니다.");
+	alert("매크로를 시작합니다.\n트럼펫 소리가 나면 5분 안에 결재를 마쳐주셔야 합니다.");
 	localStorage.setItem('macro', true);
 	inqSchedule();
 }
 
 function macrostop() {
-	alert("매크로를 중지합니다.");
+	alert("매크로를 중지합니다.\n조건을 변경하여 재조회하신 후 다시 시작하실 수 있습니다.");
 	localStorage.removeItem('macro');
 }
